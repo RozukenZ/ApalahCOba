a17594a6fbf32b5a64dd8af8ad8e7d9ec28f4f23
Lenrensenten <ahmadjauharul17@gmail.com>
2024-10-15 17:58:31 +0700

    nambahin komunitas

---
+++ AndroidStudioProjects/DemoModul1PemMob/lib/views/community.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Community extends StatefulWidget{
+  @override
+  _Community createState() => _Community();
+}
+
+class _Community extends State<Community>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Scaffold(
+
+    );
+  }
+}
